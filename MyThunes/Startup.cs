b0ad94444db3b369d70b8b4d6dd66b11253658d4
Washigeri <malek.elouerghi@gmail.com>
2017-03-23 14:48:33 +0100

    Ajoutez des fichiers projet.

---
+++ MyThunes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyThunes.Startup))]
+namespace MyThunes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
