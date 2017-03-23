0570b0572a288be7f03df8a18a6bdad92662e4b3
Washigeri <malek.elouerghi@gmail.com>
2017-03-23 16:34:38 +0100

    "model Order/Song"

---
+++ Models/Song.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public String Genre { get; set; }
+        public int Price { get; set; }
+        public String Format { get; set; }
+        public String Path { get; set; }
+
+
+    }
+}
