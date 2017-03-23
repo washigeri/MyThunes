059d2f622019ab7c79662d2128c573a71ddeefec
Pierre <Pierre@DESKTOP-5GBN4ES>
2017-03-23 16:31:23 +0100

    "Modele Album"

--- Models/Album.cs
+++ Models/Album.cs
@@ -8,5 +8,13 @@ namespace Models
 {
     class Album
     {
+        public String Name { get; set; }
+        public String Cover { get; set; }
+        public DateTime Date { get; set; }
+        //public Artist Musician { get; set; }
+        //public List<Title> Titles { get; set; }
+        public String Genre { get; set; }
+        public Decimal Price { get; set; }
+        //public List<Review> Reviews { get; set; }
     }
 }
