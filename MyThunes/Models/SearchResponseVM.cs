513fc9e30ac1a84823c29dcd2c46c9b62c341dcc
Pierre <Pierre@DESKTOP-5GBN4ES>
2017-04-13 11:38:40 +0200

    champ de recherche dans la navbar

---
+++ MyThunes/Models/SearchResponseVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyThunes.Models
+{
+    public class SearchResponseVM
+    {
+        public List<Song> Songs { get; set; }
+        public int SongsCounter { get; set; }
+        public List<Artist> Artists { get; set; }
+        public int ArtistsCounter { get; set; }
+        public List<Album> Albums { get; set; }
+        public int AlbumsCounter { get; set; }
+    }
+}
