9f738927a12242260c14c46c743f0d01a2a41827
Washigeri <malek.elouerghi@gmail.com>
2017-04-13 11:40:33 +0200

    "supression migrations"

--- MyThunes/Migrations/201704130938215_Init.cs
+++ MyThunes/Migrations/201704130938215_Init.cs
@@ -3,16 +3,14 @@ namespace MyThunes.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class supression_email : DbMigration
+    public partial class Init : DbMigration
     {
         public override void Up()
         {
-            DropColumn("dbo.Reviews", "UserEmail");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.Reviews", "UserEmail", c => c.String(nullable: false));
         }
     }
 }
